c586b4ad51df78f4b5679d4e6d3d7d61dcca87f4
alexoserna <118146045+alexoserna@users.noreply.github.com>
2023-07-14 14:00:42 -0400

    added a theme.js file and started playing with adjusting certain colors of built components

--- src/App.js
+++ src/App.js
@@ -15,32 +15,36 @@ import VanAssignments from "./pages/VanAssignments";
 import VanData from "./pages/VanData";
 import WeeklyRoster from "./pages/WeeklyRoster";
 
+import theme from './Theme';
+
 function App() {
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Sidebar />
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex' }}>
+        <Sidebar />
 
-      <Box
-        sx={{
-          marginLeft: 10,
-          transtion: 'margin-left 0.3s',
-          position: 'relative',
-          zIndex: 0,
-        }}
-      >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="Account" element={<Account />} />
-          <Route path="Associates" element={<Associates />} />
-          <Route path="DailyLog" element={<DailyLog />} />
-          <Route path="Reports" element={<Reports />} />
-          <Route path="VanAssignments" element={<VanAssignments />} />
-          <Route path="VanData" element={<VanData />} />
-          <Route path="WeeklyRoster" element={<WeeklyRoster />} />
-        </Routes>
+        <Box
+          sx={{
+            marginLeft: 10,
+            transtion: 'margin-left 0.3s',
+            position: 'relative',
+            zIndex: 0,
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="Account" element={<Account />} />
+            <Route path="Associates" element={<Associates />} />
+            <Route path="DailyLog" element={<DailyLog />} />
+            <Route path="Reports" element={<Reports />} />
+            <Route path="VanAssignments" element={<VanAssignments />} />
+            <Route path="VanData" element={<VanData />} />
+            <Route path="WeeklyRoster" element={<WeeklyRoster />} />
+          </Routes>
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
 
